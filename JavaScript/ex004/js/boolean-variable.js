270dc6e6de9f6215e0e670638ce6507490d7ce61
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-12-18 16:53:34 -0300

    array

--- JavaScript/ex004/js/boolean-variable.js
+++ JavaScript/ex004/js/boolean-variable.js
@@ -1,5 +1,8 @@
 var inStock;
 var shipping;
+
+// Variaveis do estilos boolean, tem somente dois valores, true -> verdadeiro e false -> falso
+// São muito ultil para realizar alguma validação que consiste em positiva execute parte do código e caso negativa execute outra parte do código
 inStock = true;
 shipping = false;
 
