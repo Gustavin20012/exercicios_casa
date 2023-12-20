c42cb13b6f1eb6fcfd8d6451f404a1770ec96a94
GustavoLima <47118393+Gustavin20012@users.noreply.github.com>
2023-12-19 21:50:49 -0300

    Update array-variable.js

--- JavaScript/ex005/js/array-variable.js
+++ JavaScript/ex005/js/array-variable.js
@@ -3,6 +3,11 @@ var colors;
 // Array literal
 colors = ['white', 'black', 'custom'];
 
+//Atualizar o Terceiro no Array
+colors[2] = 'beige'
 
+//Obtêm o elemento com o id colors
 var el = document.getElementById('colors');
-el.textContent = colors[0];
+
+// Substitui o terceiro item no array
+el.textContent = colors[2];
